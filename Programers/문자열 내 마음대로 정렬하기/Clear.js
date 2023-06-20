3dc792915eceb5cdec898c2b787c2e3aa2753b67
2Zerozero <hawgae@gmail.com>
2023-06-20 16:11:05 +0900

    ⚙. Problem :  문자열 내 마음대로 정렬하기 풀이 개선

--- Programers/문자열 내 마음대로 정렬하기/Clear.js
+++ Programers/문자열 내 마음대로 정렬하기/Clear.js
@@ -19,19 +19,27 @@ function solution(strings, n) {
     return answer;
 }
 
+// 다른방법 코드
+
 function solution(strings, n) {
-    return strings.sort(function (prev, next) {
+    // n번째 글자를 기준으로 오름차순 정렬
+    return strings.sort((prev, next) => {
+        // 사전순으로 문자열을 정렬하는데, 사전순으로 밀린 문자열은 뒤로 위치하게한다.
         if(prev[n] < next[n]) {
-            // 사전순으로 문자열을 정렬하는데, 사전순으로 밀린 문자열은 뒤로 위치하게한다.
-            return -1;
+            return -1
         }
+        // 사전순으로 문자열을 정렬하는데, 사전순으로 앞선 문자열은 앞으로 위치하게한다.
         if(prev[n] > next[n]) {
-            // 사전순으로 문자열을 정렬하는데, 사전순으로 앞선 문자열은 앞으로 위치하게한다.
             return 1;
         }
-        if(prev[n] === next[n]) {
-            // 문자열의 n번째 요소의 글자가 똑같다면, 문자열끼리 비교해서 위의 조건문과 똑같이 다시 실행한다.
-            return prev > next ? 1 : -1;
-        }
+        // 문자열의 n번째 요소의 글자가 똑같다면, 문자열끼리 비교해서 위의 조건문과 똑같이 다시 실행한다.
+        
+        // if(prev[n] === next[n]) {
+        //     return prev > next ? 1 : -1;
+        // }
+        
+        // localeCompare() 메서드: 참조 문자열이 정렬 순으로 지정된 문자열 앞 혹은 뒤에 오는지 또는 동일한 문자열인지 나타내는 수치를 반환.
+        return prev.localeCompare(next); 
     })
 }
+
