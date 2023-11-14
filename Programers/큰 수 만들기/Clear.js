cf790ad93ab12cae0b33871148c5a519dd28cae4
2Zerozero <hawgae@gmail.com>
2023-11-15 00:29:00 +0900

    ⚙. Problem : 큰 수 만들기 로직 설명 추가

--- Programers/큰 수 만들기/Clear.js
+++ Programers/큰 수 만들기/Clear.js
@@ -24,3 +24,40 @@ function solution(number, k) {
   answer = stack.join("");
   return answer;
 }
+
+// 예시: number = "1924", k = 2
+
+// 초기 상태: stack = [], answer = ''
+// i=0: el=1, stack=[1]
+// i=1: el=9, stack=[9]
+// i=2: el=2, stack=[9, 2]
+// i=3: el=4, stack=[9, 4] (k=1)
+// 최종 스택: [9, 4] (k=0)
+// 최종 answer: "94"
+// 예시: number = "1231234", k = 3
+
+// 초기 상태: stack = [], answer = ''
+// i=0: el=1, stack=[1]
+// i=1: el=2, stack=[2]
+// i=2: el=3, stack=[3]
+// i=3: el=1, stack=[3, 1]
+// i=4: el=2, stack=[3, 2]
+// i=5: el=3, stack=[3, 2, 3]
+// i=6: el=4, stack=[3, 2, 4] (k=2)
+// 최종 스택: [3, 2, 4] (k=0)
+// 최종 answer: "3234"
+// 예시: number = "4177252841", k = 4
+
+// 초기 상태: stack = [], answer = ''
+// i=0: el=4, stack=[4]
+// i=1: el=1, stack=[4, 1]
+// i=2: el=7, stack=[7]
+// i=3: el=7, stack=[7, 7]
+// i=4: el=2, stack=[7, 7, 2]
+// i=5: el=5, stack=[7, 7, 5]
+// i=6: el=2, stack=[7, 7, 5, 2]
+// i=7: el=8, stack=[8]
+// i=8: el=4, stack=[8, 4]
+// i=9: el=1, stack=[8, 4, 1] (k=3)
+// 최종 스택: [8, 4, 1] (k=0)
+// 최종 answer: "775841"
