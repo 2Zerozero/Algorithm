94ff9c212ad7fc172ba14b3ec06a2a654d2bdb1f
2Zerozero <hawgae@gmail.com>
2024-05-21 01:54:12 +0900

    ✨ Feat: 문제 풀이

---
+++ 제주코딩베이스캠프 자바스크립트 100제/문제11 for를 이용한 기본 활용/index.js
@@ -0,0 +1,14 @@
+/*
+  문제11 : for를 이용한 기본 활용
+  1부터 100까지 모두 더하는 Code를 for 문을 이용하여 작성하기
+*/
+
+let result = 0;
+
+// Code
+
+for (let i = 1; i <= 100; i++) {
+  result += i;
+}
+
+console.log(result);
