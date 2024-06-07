7448a1880ec3bbbf7d493643979d7c4137f8b3d8
YoungWoo Lee <hawgae@gmail.com>
2024-06-07 17:18:19 +0900

    ✨ Feat: 백준 1816

--- 알고리즘 코딩테스트/1816/index.js
+++ 알고리즘 코딩테스트/1816/index.js
@@ -1,35 +1,36 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(filePath, 'utf8').split(/\r?\n/).map();
+const readline = require('readline');
 
-const inputTestCase = [];
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// 테스트 케이스
-for (let i = 0; i < input.length; i++) {
-  console.log(`input: `, input[i]); // 테스트 값 확인
-  inputTestCase.push(+input[i]);
-}
+let input;
 
-// 솔루션
-solution(inputTestCase);
+rl.on('line', (line) => {
+  input = line.split(' ').map((v) => parseInt(v));
+  rl.close();
+});
 
-function solution(testCase) {
-  // 코드 작성란
-  const [N, ...arr] = testCase;
-  console.log(N, ...arr);
+rl.on('close', () => {
+  // 입력값이 모두 수집되면 처리 시작
+  input.forEach((numStr) => {
+    let num = BigInt(numStr); // 큰 정수를 처리하기 위해 BigInt 사용
+    let isWrongPassword = false;
 
-  for (let i = 0; i < N; i++) {
-    const element = arr[i];
-
-    for (let j = 2; j <= 1_000_000; j++) {
-      if (element % j === 0) {
-        console.log('NO');
+    for (let i = 2n; i <= 1000000n; i++) {
+      if (num % i === 0n) {
+        isWrongPassword = true;
         break;
       }
-
-      if (j === 1_000_000) {
-        console.log('YES');
-      }
     }
-  }
-}
+
+    if (isWrongPassword) {
+      console.log('NO');
+    } else {
+      console.log('YES');
+    }
+  });
+
+  process.exit();
+});
